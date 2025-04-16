9610f24841696389a30a3abfd95d78edf2103e01
Orakgoadi <oratilek960@gmail.com>
2025-04-16 18:29:53 +0200

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -1,5 +1,5 @@
 // Number of assignments/tiles
-const NUM_ASSIGNMENTS = 30;
+const NUM_ASSIGNMENTS = 44;
 
 // Generate assignment due dates (for demo: each 1 day apart from now)
 //const assignments = [];
@@ -13,26 +13,51 @@ const NUM_ASSIGNMENTS = 30;
 //  });
 //}
 const assignments = [
+    { title: "APM1513 Asign-1", due: new Date(2025, 5, 6, 11, 0, 0) },
+    { title: "APM1513 Asign-2", due: new Date(2025, 6, 6, 11, 0, 0) },
+    { title: "APM1513 Asign-3", due: new Date(2025, 7, 4, 11, 0, 0) },
+    { title: "APM1513 Asign-4", due: new Date(2025, 8, 8, 11, 0, 0) },
+    { title: "APM1514 Asign-1", due: new Date(2025, 5, 8, 11, 0, 0) },
+    { title: "APM1514 Asign-2", due: new Date(2025, 5, 22, 11, 0, 0) },
+    { title: "APM1514 Asign-3", due: new Date(2025, 6, 11, 11, 0, 0) },
+    { title: "APM1514 Asign-4", due: new Date(2025, 6, 30, 11, 0, 0) },
+    { title: "APM1514 Asign-5", due: new Date(2025, 7, 15, 11, 0, 0) },
+    { title: "APM1514 Asign-6", due: new Date(2025, 7, 29, 11, 0, 0) },
+    { title: "APM1514 Asign-7", due: new Date(2025, 8, 15, 11, 0, 0) },
+    { title: "APM1514 Asign-8", due: new Date(2025, 8, 21, 11, 0, 0) },
+    { title: "APM1514 Asign-9", due: new Date(2025, 8, 26, 11, 0, 0) },
     { title: "COS1501 Asign-1", due: new Date(2025, 5, 13, 11, 0, 0) },
     { title: "COS1501 Asign-2", due: new Date(2025, 6, 23, 11, 0, 0) },
     { title: "COS1501 Asign-3", due: new Date(2025, 7, 25, 11, 0, 0) },
     { title: "COS1501 Asign-4", due: new Date(2025, 9, 9, 9, 0, 0) },
-    { title: "INF1511 Asign-1", due: new Date(2025, 5, 25, 11, 59, 0) },
-    { title: "INF1511 Asign-2", due: new Date(2025, 7, 27, 11, 59, 0) },
-    { title: "INF1511 Asign-3", due: new Date(2025, 7, 27, 11, 59, 0) },
-    { title: "INF1511 Asign-4", due: new Date(2025, 7, 27, 11, 59, 0) },
-    { title: "COS1511 Asign-1", due: new Date(2025, 3, 16, 15, 0, 0) },
+    { title: "COS1511 Asign-1", due: new Date(2025, 4, 16, 15, 0, 0) },
     { title: "COS1511 Asign-2", due: new Date(2025, 6, 2, 11, 59, 0) },
-    { title: "COS2626 Asign-1", due: new Date(2025, 5, 7, 11, 0, 0) },
     { title: "COS1511 Asign-3", due: new Date(2025, 8, 4, 9, 0, 0) },
     { title: "COS1512 Asign-1", due: new Date(2025, 5, 20, 11, 0, 0) },
-    { title: "COS1512 Asign-2", due: new Date(2025, 7, 18, 11, 0, 0) },
-    { title: "COS1512 Asign-3", due: new Date(2025, 8, 4, 11, 0, 0) },
+    { title: "COS1512 Asign-2", due: new Date(2025, 8, 26, 11, 0, 0) },
+    { title: "COS1512 Asign-3", due: new Date(2025, 9, 9, 11, 0, 0) },
     { title: "COS1512 Asign-4", due: new Date(2025, 9, 23, 11, 0, 0) },
+    { title: "COS1521 Asign-1", due: new Date(2025, 5, 27, 11, 0, 0) },
+    { title: "COS1521 Asign-2", due: new Date(2025, 6, 17, 11, 0, 0) },
+    { title: "COS1521 Asign-3", due: new Date(2025, 7, 22, 11, 0, 0) },
+    { title: "COS1521 Asign-4", due: new Date(2025, 8, 12, 11, 0, 0) },
+    { title: "COS1521 Asign-5", due: new Date(2025, 9, 2, 11, 0, 0) },
     { title: "MAT1503 Asign-1", due: new Date(2025, 5, 22, 11, 0, 0) },
-    { title: "STA1610 Asign-1", due: new Date(2025, 5, 5, 12, 0, 0) },
-    { title: "INF1520 Asign-1", due: new Date(2025, 5, 23, 13, 0, 0) },
-    { title: "INF1505 Asign-1", due: new Date(2025, 5, 2, 13, 0, 0) },
+    { title: "MAT1503 Asign-2", due: new Date(2025, 6, 27, 11, 0, 0) },
+    { title: "MAT1503 Asign-3", due: new Date(2025, 7, 31, 11, 0, 0) },
+    { title: "MAT1503 Asign-4", due: new Date(2025, 8, 29, 11, 0, 0) },
+    { title: "MAT1503 Asign-5", due: new Date(2025, 9, 30, 11, 0, 0) },
+    { title: "MAT1512 Asign-1", due: new Date(2025, 5, 16, 11, 0, 0) },
+    { title: "MAT1512 Asign-2", due: new Date(2025, 5, 29, 11, 0, 0) },
+    { title: "MAT1512 Asign-3", due: new Date(2025, 6, 13, 11, 0, 0) },
+    { title: "MAT1512 Asign-4", due: new Date(2025, 6, 30, 11, 0, 0) },
+    { title: "MAT1512 Asign-5", due: new Date(2025, 7, 18, 11, 0, 0) },
+    { title: "MAT1512 Asign-6", due: new Date(2025, 7, 31, 11, 0, 0) },
+    { title: "MAT1613 Asign-1", due: new Date(2025, 5, 2, 11, 0, 0) },
+    { title: "MAT1613 Asign-2", due: new Date(2025, 6, 27, 11, 0, 0) },
+    { title: "MAT1613 Asign-3", due: new Date(2025, 8, 8, 11, 0, 0) },
+    { title: "MAT1613 Asign-4", due: new Date(2025, 9, 26, 11, 0, 0) },
+    
     // you can add as many titles as you want as long as you change the variable "const NUM_ASSIGNMENTS = 30;"
   ];
   
@@ -84,3 +109,4 @@ function updateCountdowns() {
 // Start the countdown timer
 updateCountdowns();
 setInterval(updateCountdowns, 1000);
+
